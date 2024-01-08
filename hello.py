9c0c8f781dd5873170a6d4377541945b17bd7cc8
Sonja Rohleder <sonja.rohleder@postgrad.manchester.ac.uk>
2024-01-08 20:02:50 +0100

    Initial commit

---
+++ hello.py
@@ -0,0 +1,15 @@
+"""
+A simple hello world script
+"""
+
+
+def print_hi(name):
+    """
+    A simple function that takes a text string variable, and uses it in string formatting which is printed
+    """
+    print(f'Hi, {name}')
+
+
+string = "Sonja"
+print_hi(string)
+
